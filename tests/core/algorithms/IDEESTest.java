40e00909cd58672d9c074850444633918e9bfe0a
Lior <agraded@gmail.com>
2020-02-16 21:55:10 +0200

    IDEES + Tests Pancakes FULLY FUNCTIONAL.

--- tests/core/algorithms/IDEESTest.java
+++ tests/core/algorithms/IDEESTest.java
@@ -1,19 +1,49 @@
 package core.algorithms;
 
 import core.*;
-import core.domains.FifteenPuzzleTest;
 import core.domains.OverrideDomain;
 import core.domains.Pancakes;
 import core.generators.UniversalGenerator;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.SQLOutput;
+import java.util.List;
+
 public class IDEESTest {
 
+    /*
     public static void main(String[] args){
-        PuzzleTest();
-    }
+        String PATH = System.getProperty("user.dir") + "\\testResources\\core\\domains\\BugPancakes.txt";
+        try{
+            InputStream inStream = new FileInputStream(new File(PATH));
+            Pancakes domain = new Pancakes(inStream);
 
-    public static void PuzzleTest(){
+            System.out.println("WIDA*-------------------------------------------");
+            SearchAlgorithm ida = new IDAstar(1.5);
+            SearchResult IDAstarRes = ida.search(domain);
+            System.out.println(IDAstarRes);
+
+//            State init = domain.initialState();
+//            Operator op = domain.getOperator(init, 4);
+//            System.out.println(op.toString());
+//            System.out.println(domain.applyOperator(init,op).convertToString());
+
+            System.out.println("IDEES-------------------------------------------");
+            SearchAlgorithm idees = new IDEES(1.5);
+            SearchResult ideesRes = idees.search(domain);
+            System.out.println(ideesRes);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    */
+
+    @Test
+    public void PuzzleTest(){
         Pancakes domain = new Pancakes(25);
         UniversalGenerator universal = new UniversalGenerator();
         double avgDeltaExpanded = 0;
@@ -21,19 +51,18 @@ public class IDEESTest {
         double avgDeltaSolutionLen = 0;
         int runs = 100;
         for(int i=0;i<runs;i++){
-            System.out.println("****ITERATION " + i + "****");
             State newState = universal.generate(domain,50);
             domain.setInitialState(newState);
-
-            System.out.println("WIDA*-------------------------------------------");
-            SearchAlgorithm ida = new IDAstar(1.5);
-            SearchResult IDAstarRes = ida.search(domain);
-            System.out.println(IDAstarRes);
 
             System.out.println("IDEES-------------------------------------------");
             SearchAlgorithm idees = new IDEES(1.5);
             SearchResult ideesRes = idees.search(domain);
             System.out.println(ideesRes);
+
+            System.out.println("WIDA*-------------------------------------------");
+            SearchAlgorithm ida = new IDAstar(1.5);
+            SearchResult IDAstarRes = ida.search(domain);
+            System.out.println(IDAstarRes);
 
             /*System.out.println("IDPS-------------------------------------------");
             SearchAlgorithm idps = new ImprovingPS(1.5);
@@ -44,38 +73,10 @@ public class IDEESTest {
             avgDeltaGenerated += IDAstarRes.getGenerated()-ideesRes.getGenerated();
             avgDeltaSolutionLen += IDAstarRes.getSolutions().get(0).getLength()-ideesRes.getSolutions().get(0).getLength();
         }
-//
 
         System.out.println("AVG Delta Expanded: "+avgDeltaExpanded/runs);
         System.out.println("AVG Delta Generated: "+avgDeltaGenerated/runs);
         System.out.println("AVG Delta Solution Length: "+avgDeltaSolutionLen/runs);
 
-    }
-
-
-    public void G1Test(){
-        OverrideDomain domain = Graphs.graph1;
-        SearchAlgorithm solver = new IDEES();
-        SearchResult result = solver.search(domain);
-        Solution sol = result.getSolutions().get(0);
-        TestAllBasics.showSolution(result,0);
-    }
-
-
-    public void G2Test(){
-        OverrideDomain domain = Graphs.graph2;
-        SearchAlgorithm solver = new IDEES(2);
-        SearchResult result = solver.search(domain);
-        Solution sol = result.getSolutions().get(0);
-        TestAllBasics.showSolution(result,0);
-    }
-
-
-    public void G3Test(){
-        OverrideDomain domain = Graphs.graph3;
-        SearchAlgorithm solver = new IDEES(2);
-        SearchResult result = solver.search(domain);
-        Solution sol = result.getSolutions().get(0);
-        TestAllBasics.showSolution(result,0);
     }
 }
