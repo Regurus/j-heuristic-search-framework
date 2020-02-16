a8dbf04996c1c0d4e2e701457caa2e86e57fc082
Lior <agraded@gmail.com>
2020-02-16 14:22:23 +0200

    IDEES + IDEES Test file added

---
+++ tests/core/algorithms/IDEESTest.java
@@ -0,0 +1,72 @@
+package core.algorithms;
+
+import core.*;
+import core.domains.OverrideDomain;
+import core.domains.Pancakes;
+import core.generators.UniversalGenerator;
+import org.junit.Test;
+
+public class IDEESTest {
+
+    @Test
+    public void PuzzleTest(){
+        Pancakes domain = new Pancakes(25);
+        UniversalGenerator universal = new UniversalGenerator();
+        long avgDeltaExpanded = 0;
+        long avgDeltaGenerated = 0;
+        long avgDeltaSolutionLen = 0;
+        int runs = 100;
+        for(int i=0;i<runs;i++){
+            State newState = universal.generate(domain,50);
+            domain.setInitialState(newState);
+
+            System.out.println("IDEES-------------------------------------------");
+            SearchAlgorithm idees = new IDEES(1.5);
+            SearchResult ideesRes = idees.search(domain);
+            System.out.println(ideesRes);
+
+            System.out.println("WIDA*-------------------------------------------");
+            SearchAlgorithm ida = new IDAstar(1.5);
+            SearchResult IDAstarRes = ida.search(domain);
+            System.out.println(IDAstarRes);
+
+            /*System.out.println("IDPS-------------------------------------------");
+            SearchAlgorithm idps = new ImprovingPS(1.5);
+            SearchResult IDPSRes = idps.search(domain);
+            System.out.println(IDPSRes);*/
+
+            avgDeltaExpanded += IDAstarRes.getExpanded()-ideesRes.getExpanded();
+            avgDeltaGenerated += IDAstarRes.getGenerated()-ideesRes.getGenerated();
+            avgDeltaSolutionLen += IDAstarRes.getSolutions().get(0).getLength()-ideesRes.getSolutions().get(0).getLength();
+        }
+
+        System.out.println("AVG Delta Expanded: "+avgDeltaExpanded/runs);
+        System.out.println("AVG Delta Generated: "+avgDeltaGenerated/runs);
+        System.out.println("AVG Delta Solution Length: "+avgDeltaSolutionLen/runs);
+
+    }
+    @Test
+    public void G1Test(){
+        OverrideDomain domain = Graphs.graph1;
+        SearchAlgorithm solver = new IDEES();
+        SearchResult result = solver.search(domain);
+        Solution sol = result.getSolutions().get(0);
+        TestAllBasics.showSolution(result,0);
+    }
+    @Test
+    public void G2Test(){
+        OverrideDomain domain = Graphs.graph2;
+        SearchAlgorithm solver = new IDEES(2);
+        SearchResult result = solver.search(domain);
+        Solution sol = result.getSolutions().get(0);
+        TestAllBasics.showSolution(result,0);
+    }
+    @Test
+    public void G3Test(){
+        OverrideDomain domain = Graphs.graph3;
+        SearchAlgorithm solver = new IDEES(2);
+        SearchResult result = solver.search(domain);
+        Solution sol = result.getSolutions().get(0);
+        TestAllBasics.showSolution(result,0);
+    }
+}
