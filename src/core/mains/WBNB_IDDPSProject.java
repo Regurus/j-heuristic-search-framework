64500abe71b08f9e6a421d5fcab24bc725ca80e3
Regurus <weltall2004@gmail.com>
2020-02-29 19:29:48 +0200

    Main WBnB project done

--- src/core/mains/WBNB_IDDPSProject.java
+++ src/core/mains/WBNB_IDDPSProject.java
@@ -1,17 +1,18 @@
 package core.mains;
 
 import core.SearchAlgorithm;
+import core.SearchDomain;
 import core.SearchResult;
 import core.State;
 import core.algorithms.IDAstar;
+import core.algorithms.IDDPS;
 import core.domains.FifteenPuzzle;
 import core.domains.Pancakes;
+import core.domains.VacuumRobot;
 import core.generators.UniversalGenerator;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -19,77 +20,125 @@ import java.util.Stack;
 public class WBNB_IDDPSProject {
     private static final int DUMP_PERIOD=20;
     private static final int iterations = 50;
-    private static final int PAN_DEPTH = 50;
-    private static final int FIFTEEN_DEPTH = 50;
-    private static String VACUUM_FOLDER = "testResources/core/domains/VacuumRobotTestFiles";
+    private static final String VACUUM_FOLDER = "testResources/core/domains/VacuumRobotTestFiles";
+    private static final String PANCAKES_FOLDER = "testResources/core/domains/PancakeTestFiles";
+    private static final String FIFTEEN_FOLDER = "testResources/core/domains/FifteenPuzzleInstances";
     private static String[] VACCUM_FILES;
+    private static String[] PANCAKE_FILES;
+    private static String[] FIFTEEN_FILES;
+
 
     public static void main(String[] args) {
         File f = new File(VACUUM_FOLDER);
         VACCUM_FILES = f.list();
-
-        for(double i=1.0; i<3.0; i+=0.1){
+        f = new File(PANCAKES_FOLDER);
+        PANCAKE_FILES = f.list();
+        f = new File(FIFTEEN_FOLDER);
+        FIFTEEN_FILES = f.list();
+        for(double i=2.0; i<3.0; i+=0.1){
             singleWeight(i,iterations);
         }
 
     }
 
     public static void singleWeight(double weight,int iterations){
-        //TODO: plug multithreading here
-        IDAstar idAstar = new IDAstar(weight);
-        runAlgorithm(idAstar, iterations,weight);/*
-        WBNB Wbnb = new WBNB(weight);
-        runAlgorithm(Wbnb, iterations,weight);
-        IDDPS iddps = new IDDPS(weight);
-        runAlgorithm(iddps, iterations,weight);*/
+        Runnable task1 = () -> {
+            IDAstar idAstar = new IDAstar(weight);
+            try {
+                runAlgorithm(idAstar, weight);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread t1 = new Thread(task1);
+        System.out.println("Thread 1 started IDA* run on weight: "+weight);
+        t1.start();
+        /*Runnable task2 = () -> {
+            WBNB Wbnb = new WBNB(weight);
+            try {
+
+                runAlgorithm(Wbnb, iterations,weight);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread t2 = new Thread(task2);
+        System.out.println("Thread 2 started WBnB run on weight: "+weight);
+        t2.start();*/
+        Runnable task3 = () -> {
+            IDDPS iddps = new IDDPS(weight);
+            try {
+                runAlgorithm(iddps, weight);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread t3 = new Thread(task3);
+        System.out.println("Thread 3 started IDDPS run on weight: "+weight);
+        t3.start();
+        try {
+            t1.join();
+            System.out.println("IDA* run finished on weight: "+weight);
+            //t2.join();
+            //System.out.println("WBnB run finished on weight: "+weight);
+            t3.join();
+            System.out.println("IDDPS run finished on weight: "+weight);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    private static void runAlgorithm(SearchAlgorithm algorithm,int iterations,double weight){
+    private static void runAlgorithm(SearchAlgorithm algorithm,double weight) throws FileNotFoundException {
         Stack<List> lists = new Stack<>();
         Stack<String> fileNames = new Stack<>();
         //fifteen puzzle initiation
-        FifteenPuzzle fifteenPuzzle = new FifteenPuzzle();
-        String fifteeenName = algorithm.getClass().getSimpleName()+"_"+weight+"_FifteenPuzzle.csv";
+        String fifteeenName = algorithm.getClass().getSimpleName()+"_"+new DecimalFormat("##.##").format(weight)+"_FifteenPuzzle.csv";
         ArrayList<String> fifteenResults = new ArrayList<>();
         lists.push(fifteenResults);
         fileNames.push(fifteeenName);
         //pancakes initiation
-        Pancakes pancakes = new Pancakes(50);
-        String panName = algorithm.getClass().getSimpleName()+"_"+weight+"_Pancakes.csv";
+        String panName = algorithm.getClass().getSimpleName()+"_"+new DecimalFormat("##.##").format(weight)+"_Pancakes.csv";
         ArrayList<String> pancakesResults = new ArrayList<>();
         lists.push(pancakesResults);
         fileNames.push(panName);
+        //vacuum initiation
+        String vacuumName = algorithm.getClass().getSimpleName()+"_"+new DecimalFormat("##.##").format(weight)+"_Vacuum.csv";
+        ArrayList<String> vacuumResults = new ArrayList<>();
+        lists.push(vacuumResults);
+        fileNames.push(vacuumName);
         //optimal checker
-        IDAstar idAstar = new IDAstar();
-        final int maxDepth = 50;
-        UniversalGenerator universalGenerator = new UniversalGenerator();
-
-        for(int i=1;i<iterations;i++){
-            //fifteen puzzle
-            State initial = universalGenerator.generate(fifteenPuzzle,maxDepth);
-            fifteenPuzzle.setInitialState(initial);
-            SearchResult fifteenResult = algorithm.search(fifteenPuzzle);
-            SearchResult optimal = idAstar.search(fifteenPuzzle);
-            String line = ""+i+","+fifteenResult.getExpanded()+","+fifteenResult.getGenerated()+","
-                    +fifteenResult.getCpuTimeMillis()+","+fifteenResult.getWallTimeMillis()+
-                    ","+fifteenResult.getSolutions().get(0).getLength()+","+optimal.getSolutions().get(0).getLength()+","+initial.convertToStringShort();
-            fifteenResults.add(line);
-            System.out.println("FifteenPuzzle: "+line);
-            //pancakes
-            initial = universalGenerator.generate(pancakes,maxDepth);
-            pancakes.setInitialState(initial);
-            SearchResult pancakesResult = algorithm.search(pancakes);
-            optimal = idAstar.search(pancakes);
-            line = ""+i+","+pancakesResult.getExpanded()+","+pancakesResult.getGenerated()+","
-                    +pancakesResult.getCpuTimeMillis()+","+pancakesResult.getWallTimeMillis()+
-                    ","+pancakesResult.getSolutions().get(0).getLength()+","+optimal.getSolutions().get(0).getLength()+","+initial.convertToStringShort();
-            pancakesResults.add(line);
-            System.out.println("Pancakes: "+line);
-            if(i%DUMP_PERIOD==0)
-                dumpToFile(lists,fileNames);
-
+        if(VACCUM_FILES.length!=FIFTEEN_FILES.length||VACCUM_FILES.length!=PANCAKE_FILES.length){
+            System.out.println("Same amount of instances required!!!");
+            return;
+        }
+        for(String fileName:VACCUM_FILES){
+            //Fifteen puzzle
+            SearchDomain domain = new FifteenPuzzle(new FileInputStream(FIFTEEN_FOLDER+"/"+fileName));
+            String line = runOnDomain(domain, algorithm, fileName);
+            System.out.println("Fifteen Puzzle: "+line);
+            vacuumResults.add(line);
+            //Pancake puzzle
+            domain = new Pancakes(new FileInputStream(PANCAKES_FOLDER+"/"+fileName));
+            line = runOnDomain(domain, algorithm, fileName);
+            System.out.println("Pancake Puzzle: "+line);
+            vacuumResults.add(line);
+            //Vacuum
+            domain = new VacuumRobot(new FileInputStream(VACUUM_FOLDER+"/"+fileName));
+            line = runOnDomain(domain, algorithm,fileName);
+            System.out.println("Vacuum Robot: "+line);
+            vacuumResults.add(line);
         }
         dumpToFile(lists,fileNames);
+    }
+
+    private static String runOnDomain(SearchDomain domain, SearchAlgorithm algorithm,String fileName){
+        IDAstar idAstar = new IDAstar();
+        SearchResult vacuumResult = algorithm.search(domain);
+        SearchResult optimal = idAstar.search(domain);
+        return ""+fileName+","+vacuumResult.getExpanded()+","+vacuumResult.getGenerated()+","
+                +vacuumResult.getCpuTimeMillis()+","+vacuumResult.getWallTimeMillis()+
+                ","+vacuumResult.getSolutions().get(0).getLength()+","+optimal.getSolutions().get(0).getLength();
     }
 
     private static void dumpToFile(Stack<List> results, Stack<String> filenames){
